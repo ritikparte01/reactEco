8e9c17decffc008e6562d40fc42f778e856a208b
Ritik Parte <ritik.parte@vernost.in>
2024-03-15 21:13:35 +0530

    Product Detail Pgae Discount & Payment functionality

--- src/Components/ProdDetail.js
+++ src/Components/ProdDetail.js
@@ -2,17 +2,39 @@ import React, { useEffect, useState } from "react";
 import './prodDetail.css'
 import axios from "axios";
 import Footer from "./Footer";
-
+import Swal from 'sweetalert2'
 
 function ProdDetail() {
     const [singleProduct, setSingleProduct] = useState("");
     const [prodID, setProdID] = useState("");
     const [catId, setCatId] = useState("");
     const [similerProduct, setSimilerProduct] = useState([]);
+    const [singlePrice, setSinglePrice] = useState("");
+    const [DiscountedPrice, setDiscountedPrice] = useState("");
+    const [discount, setDiscount] = useState("");
+    const [userName, setUserName] = useState("");
+    const [paymentID, setPaymentID] = useState("");
 
     console.log(localStorage.getItem('buyBtnID'));
 
+    const localToken = localStorage.getItem('userToken');
 
+    useEffect(() => {
+        axios({
+            method: 'GET',
+            url: 'https://api.escuelajs.co/api/v1/auth/profile',
+            headers: {
+                Authorization: `Bearer ${localToken}`,
+            }
+        }).then((res) => {
+            setUserName(res.data.name);
+        })
+    }, [])
+
+    useEffect(() => {
+        setDiscount(singlePrice / 100 * 20)
+        setDiscountedPrice(singlePrice - discount);
+    })
 
     useEffect(() => {
         setProdID(localStorage.getItem('buyBtnID'));
@@ -28,6 +50,8 @@ function ProdDetail() {
                     console.log("Single Product:", response.data);
                     setCatId(response.data.category.id);
                     console.log('Catego ID', catId);
+                    setSinglePrice(response.data.price * 10);
+                    console.log(singlePrice, 'Single Price 10');
                 })
                 .catch(error => {
                     console.error("Error fetching product:", error);
@@ -48,6 +72,76 @@ function ProdDetail() {
                 });
         }
     }, [catId]);
+
+
+
+    // DEMO RAZORPAY
+
+    const loadScript = (src) => {
+        return new Promise((resolve) => {
+            const script = document.createElement('script');
+            script.src = src;
+
+            script.onload = () => {
+                resolve(true);
+            };
+
+            script.onerror = () => {
+                resolve(false);
+            };
+
+            document.body.appendChild(script);
+        });
+    };
+
+    const initializeRazorpay = (DiscountedPrice) => {
+        const options = {
+            key: "rzp_test_0ZMfbjrmGjb8xt",
+            currency: "INR",
+            amount: DiscountedPrice * 100,
+            name: `${userName}`,
+            description: 'Thanks',
+            handler: function (response) {
+                // alert(response.razorpay_payment_id);
+                // alert("Payment successful");
+                console.log(response);
+                setPaymentID(response.razorpay_payment_id);
+                Swal.fire({
+                    title: "Payment successful",
+                    text: `Payment Reciept ID:  ${response.razorpay_payment_id}`,
+                    icon: "success"
+                })
+
+            }
+        };
+
+        const paymentObject = new window.Razorpay(options);
+        paymentObject.open();
+    };
+
+
+    // const [DiscountedPrice, setDiscountedPrice] = useState(0);
+
+    const handleProceedToPay = () => {
+        loadRazorpayScript();
+    };
+
+    const loadRazorpayScript = async () => {
+        const loaded = await loadScript('https://checkout.razorpay.com/v1/checkout.js');
+        if (loaded) {
+            console.log('Razorpay script loaded successfully');
+            initializeRazorpay(DiscountedPrice);
+        } else {
+            console.error('Failed to load Razorpay script');
+        }
+    };
+
+    useEffect(() => {
+        // Your useEffect will be triggered only when the 'DiscountedPrice' state changes
+        // (assuming it's updated when the user selects items or performs relevant actions)
+    }, [DiscountedPrice]);
+
+    // RAZOR PAY END 
 
 
 
@@ -72,7 +166,7 @@ function ProdDetail() {
                                 </div>
                             </div>
                             <div className="col-md-7">
-                                <div className="main-description px-2">
+                                <div className="main-description">
                                     <div className="category text-bold">
                                         <span>Category:</span> {singleProduct?.category?.name}
                                     </div>
@@ -81,18 +175,32 @@ function ProdDetail() {
                                     </div>
 
 
-                                    <div className="price-area my-4">
-                                        <p className="old-price mb-1"><del>₹ 100</del> <span className="old-price-discount text-danger">(20% off)</span></p>
-                                        <p className="new-price text-bold mb-1">₹ {singleProduct.price * 10}</p>
-                                        <p className="text-secondary mb-1">(Additional tax may apply on checkout)</p>
+                                    <div className="price-area my-3">
+                                        <p className="old-price mb-1"><del>₹ {singlePrice}</del> <span className="old-price-discount text-danger">(20% off)</span></p>
+                                        <p className="new-price text-bold mb-1">₹ {DiscountedPrice}</p>
+                                        <p className="text-secondary mb-1 info_text">(Additional tax may apply on checkout)</p>
 
                                     </div>
 
                                 </div>
 
                                 <div className="product-details my-4">
-                                    <p className="details-title text-color mb-1">Product Details</p>
+                                    <p className="details-title text-color mb-1 text-dark text-bold fw-bold">Product Details</p>
                                     <p className="description">{singleProduct.description}</p>
+                                </div>
+
+                                <div>
+
+                                    <select className="size_selector">
+                                        <option value="Null">Select Size</option>
+                                        <option value="xs">Xs</option>
+                                        <option value="s">S</option>
+                                        <option value="m">M</option>
+                                        <option value="l">L</option>
+                                        <option value="xl">Xl</option>
+                                        <option value="xxl">Xxl</option>
+                                    </select>
+
                                 </div>
 
 
@@ -110,7 +218,7 @@ function ProdDetail() {
                                         <button className="btn btn-dark px-4 py-2">Add to cart</button>
                                     </div>
                                     <div className="block">
-                                        <a href="#" className="shadow btn btn-success px-4 py-2">Proceed To Pay</a>
+                                        <a href="#" className="shadow btn btn-success px-4 py-2" onClick={() => handleProceedToPay(DiscountedPrice.toFixed(2))}>Proceed To Pay</a>
                                     </div>
                                 </div>
                             </div>
